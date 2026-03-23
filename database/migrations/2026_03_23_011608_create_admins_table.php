7ef4485e21e95b6bf3c3d7b1b051b7df7daee53c
kevin <kevinkalenga10@gmail.com>
2026-03-23 02:50:27 +0100

    Multi Authentication Using Multi Guard - Part2

--- database/migrations/2026_03_23_011608_create_admins_table.php
+++ database/migrations/2026_03_23_011608_create_admins_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('password');
-            $table->string('photo');
-            $table->string('token')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('token');
             $table->timestamps();
         });
     }
